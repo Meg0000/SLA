37d538471784e42e7bd4a924129ead99b3eab78d
Megan0x <synkron@protonmail.com>
2024-09-23 20:23:16 +0200

    POM - reorganize code

--- src/base_page.py
+++ src/base_page.py
@@ -1,3 +1,5 @@
+import time
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -5,15 +7,27 @@ from selenium.webdriver.common import keys
 import loctrs
 
 
-class LoginPage:
-    def __init__(self, driver):
-        self.driver = driver
+class BasePage:
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--disable-search-engine-choice-screen")
+        self.options.add_argument("disable-infobars")
+        self.driver = webdriver.Chrome(options=self.options)
+
+
+class LoginPage(BasePage):
+    
+    def __init__(self):
+        super().__init__()
         self.locator = loctrs.LoginLoc
 
     def login(self, password, username):
+        self.driver.get("https://www.saucedemo.com/")
         self.driver.find_element(*self.locator.PASSWORD).send_keys(password)
         self.driver.find_element(*self.locator.USERNAME).send_keys(username)
         self.driver.find_element(*self.locator.LOGIN_BUTTON).click()
+        time.sleep(3)
 
 
-
+t1 = LoginPage()
+t1.login("secret_sauce", "standard_user")
