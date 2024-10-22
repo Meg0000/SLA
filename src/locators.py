d45ab13c33c8530ca7166baec0eb5750845438e8
Megan0x <synkron@protonmail.com>
2024-10-22 21:29:52 +0200

    -added CartLocators class, -two more to-do's added, -check test_selenium folder for data-testid tips

--- src/locators.py
+++ src/locators.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class LoginLocators:
-
     USERNAME = (By.NAME, "user-name")
     PASSWORD = (By.NAME, "password")
     LOGIN_BUTTON = (By.NAME, "login-button")
@@ -11,8 +10,15 @@ class LoginLocators:
 
 
 class InventoryLocators:
-
     SAUCE_LABS_BACKPACK_TO_CART = (By.NAME, "add-to-cart-sauce-labs-backpack")
     SHOPPING_CART_ITEM_COUNT = (By.CSS_SELECTOR, '[data-test="shopping-cart-badge"]')
+    # todo: get all items names locators>append to list
 
+
+class CartLocators:
+    REMOVE_BUTTON = (By.ID, "remove-sauce-labs-backpack")
+    CHECKOUT_BUTTON = (By.ID, "checkout")
+    CONTINUE_SHOPP_BUTTON = (By.ID, "continue-shopping")
+    GO_TO_ITEM = None
+    # todo: how to use data-test id
 
